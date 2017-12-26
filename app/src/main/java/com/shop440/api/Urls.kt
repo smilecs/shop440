27005891f44de0c855fe5a76137903c02c84154f
smilecs <mumene@gmail.com>
2017-12-26 09:42:00 +0100

    added viewpager indicator

--- app/src/main/java/com/shop440/api/Urls.kt
+++ app/src/main/java/com/shop440/api/Urls.kt
@@ -7,8 +7,8 @@ package com.shop440.api
 object Urls {
     //const val BASE_URL = "http://192.168.1.59:8080"
     const val NEW_USER = "/api/users/signup"
-    //const val BASE_URL = "http://192.168.8.101:8080"
-    const val BASE_URL = "https://shop440.com"
+    const val BASE_URL = "http://192.168.8.104:8080"
+    //const val BASE_URL = "https://shop440.com"
     const val LOGIN = "/api/users/login"
     const val CHECKPHONE = "/api/users/checkphonenumber"
     const val HOME_PAGE_SECTION = "/api/home/sections"
