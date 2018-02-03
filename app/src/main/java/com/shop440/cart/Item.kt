d9bf9fcb5eff36ae9d79dec1a5d0320154590847
smilecs <mumene@gmail.com>
2018-02-03 08:59:33 +0100

    added shop details to item model

--- app/src/main/java/com/shop440/cart/Item.kt
+++ app/src/main/java/com/shop440/cart/Item.kt
@@ -6,7 +6,7 @@ import io.realm.RealmObject
 /**
  * Created by SMILECS on 1/7/18.
  */
-open class Item(@SerializedName(value = "total_price") var totalPrice: Double,
-                @SerializedName(value = "slug") var slug: String) : RealmObject(){
-    constructor() : this(0.0, "")
+ open class Item(@SerializedName(value = "total_price") var totalPrice: Double,
+                @SerializedName(value = "slug") var slug: String, var shopName:String, var shopSlug:String) : RealmObject(){
+    constructor() : this(0.0, "", "", "")
 }
